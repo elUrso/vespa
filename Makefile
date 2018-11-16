dc6a4a8477a0c21cca17b56f2db2754e83275bd9
Vitor Silva <sirmochi@Vitors-iMac.local>
2018-11-16 13:43:50 -0200

    Added test files

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
 CC=gcc
 FLAGS=-g -O3 -Wall
 OBJS=m.o str.o list.o
+TESTS=list.test.o
 
 vespa: $(OBJS)
 	$(CC) main.c $(OBJS) $(FLAGS) -o vespa
 
 %.o: %.c
 	$(CC) -c -o $@ $< $(FLAGS)
+
+test: $(OBJS) $(TESTS)
+	$(CC) list.test.o $(OBJS) -o test.list
+	./test.list && echo "List Passed" || echo "List Failed";
+	rm test*
+clear:
+	rm *.o
+
--- list.test.c
+++ list.test.c
@@ -6,8 +6,11 @@
 
 int main(int argc, char **argv) {
     MObj * l = Mnew(MList, NULL);
-    printf("%lld\n", (long long int) l);
+    MObj * t = 0;
+    /*printf("%lld\n", (long long int) l);*/
     Mcall(l, "insert", Mpack(l, "MObj", Mpack("self", "str", NULL)));
-    printf("%lld\n", (long long int) Mcall(l, "query", Mpack("self", "str", NULL)));
+    t = Mcall(l, "query", Mpack("self", "str", NULL));
+    /*printf("%lld\n", (long long int) t);*/
+    if(t != l) exit(EXIT_FAILURE);
     return 0;
 }
